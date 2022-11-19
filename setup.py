6c449d942c14506242610fd661106a86c4206f0d
jizong <jizong.peng.1@etsmtl.net>
2022-11-19 09:41:29 -0500

    fixed dependency in setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='foxmetrics',
-    version='1.1.3',
+    version='1.1.5',
     packages=['foxmetrics'],
     url='',
     license='',
     author='jizong',
     author_email='jizong.peng.ca@gmail.com',
     description='metrics for decoupled training',
-    requires=["loguru", "termcolor"],
+    install_requires=["loguru", "termcolor", "pandas"],
     python_requires='>3.6.0'
 )
