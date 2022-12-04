ddf4486b2383342358726e525c044b90e6b279c5
jizong <jizong.peng.1@etsmtl.net>
2022-12-04 17:40:31 -0500

    improved distributed env

--- foxhelpers/distributed_helper.py
+++ foxhelpers/distributed_helper.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from torch import distributed as dist
 
 
-def is_distributed() -> bool:
+def is_dist_initialized() -> bool:
     try:
         return dist.is_initialized()
     except:
@@ -13,19 +13,36 @@ def is_distributed() -> bool:
 
 @dataclass
 class DistributedEnv:
-    is_distributed: bool = is_distributed()
-    local_rank: int = os.environ.get("LOCAL_RANK", 0)
-    word_size: int = os.environ.get("WORLD_SIZE", 1)
+    local_rank: int = int(os.environ.get("LOCAL_RANK", 0))
+    word_size: int = int(os.environ.get("WORLD_SIZE", 1))
 
     @property
     def on_master(self) -> bool:
-        if is_distributed():
-            return self.local_rank == 0
-        return True
+        return self.local_rank == 0
+
+    @property
+    def is_distributed(self) -> bool:
+        """this flag can change during the code"""
+
+        return self.word_size > 1
+
+    @property
+    def is_dist_initialized(self) -> bool:
+        return is_dist_initialized()
 
     def barrier(self) -> None:
-        if self.is_distributed:
+        if self.is_dist_initialized:
             dist.barrier()
 
     def ipdb_set_trace(self):
-        import ipdb
+        if not self.is_distributed:
+            import ipdb
+            ipdb.set_trace()
+        elif not self.is_dist_initialized:
+            # disable the input method for other processs
+            raise RuntimeError("distributed training not initialized yet.")
+        else:
+            if self.on_master:
+                import ipdb
+                ipdb.set_trace()
+            dist.barrier()
